22d4b449756806aea25f71342425f82b516dfb18
Bartosz Wnorowski <wnbartek@wp.pl>
2019-12-19 20:43:56 +0100

    delete, get and list

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM chaberb/flask-rest
 ADD . /var/www/app
-#WORKDIR /var/www/app
+
 RUN pip install -r /var/www/app/requirements.txt
 # CMD ["python", "app.py"]
+#WORKDIR /var/www/app
--- app.py
+++ app.py
@@ -11,8 +11,13 @@ app = Flask(__name__)
 HAL(app)  # Initialise HAL
 n_pub = 0
 
-@app.route('/add', methods = ['POST'])
-def hello():
+@app.route('/publications/', methods = ['POST'])
+def addPub():
+    auth = request.headers['Authorization']
+    auth = auth.split(':')
+    if(auth[0]!='user' or auth[1]!='password'):
+        return 'Wrong authorization data', 400
+
     n_pub = redis.get('n_pub')
     if(n_pub == None):
         redis.set('n_pub', '0')
@@ -20,19 +25,47 @@ def hello():
     n_pub = int(n_pub) 
     n_pub+=1
     redis.set('n_pub', n_pub)
-    key = request.form['username'] + '/' + str(n_pub)
+    key = auth[0] + '/' + str(n_pub)
+
     doc = document.Document(data={
         'author' : request.form['author'],
         'title' : request.form['title']
     })
     binary = pickle.dumps(doc)
     redis.set(key, binary)
+    
     uncoded = pickle.loads(redis.get(key))
-    return uncoded
+    return uncoded.data['author'], 201
 
-@app.route('/test', methods = ['POST'])
+@app.route('/publications/<id>', methods = ['GET'])
+def getPub(id):
+    auth = request.headers['Authorization']
+    auth = auth.split(':')
+    if(auth[0]!='user' or auth[1]!='password'):
+        return 'Wrong authorization data', 400
+    key = auth[0] + '/' + str(id)
+    pub = redis.get(key)
+    if (pub is None):
+        return "Wrong publication id", 404
+    return pickle.loads(pub)
+
+@app.route('/publications/<id>', methods = ['DELETE'])
+def delPub(id):
+    auth = request.headers['Authorization']
+    auth = auth.split(':')
+    if(auth[0]!='user' or auth[1]!='password'):
+        return 'Wrong authorization data', 400
+    key = auth[0] + '/' + str(id)
+    resp = redis.delete(key)
+    return str(resp)
+
+@app.route('/publications', methods = ['GET'])
 def start():
-    return request.form['author']
+    ret = redis.keys("user*")
+    pubs = redis.mget(ret)
+    pub = pickle.loads(pubs[0])
+
+    return pub
 
 
 if __name__ == "__main__":
